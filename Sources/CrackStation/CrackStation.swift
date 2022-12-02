916fc381b595313bb3b47dcbee5825697a5e62f4
Souvik Das <souvikdas2411@gmail.com>
2022-12-02 06:55:10 -0800

    updated hashDict to a let constant

--- Sources/CrackStation/CrackStation.swift
+++ Sources/CrackStation/CrackStation.swift
@@ -3,21 +3,14 @@ import CryptoKit
 
 public class CrackStation: Decrypter {
     
-    private var hashDict:[String:String]
+    private let hashDict:[String:String]
     
     public required init() {
-        self.hashDict = [:]
-        if let path = Bundle.module.url(forResource: "data", withExtension: "json") {
-            do {
-                let data = try Data(contentsOf: path)
-                let jsonResult = try JSONSerialization.jsonObject(with: data)
-                if let jsonResult = jsonResult as? [String:String] {
-                    self.hashDict = jsonResult
-                }
-            } catch {
-                self.hashDict = [:]
-            }
-        }
+        let path = Bundle.module.url(forResource: "data", withExtension: "json")!
+        let data = try! Data(contentsOf: path)
+        let jsonResult = try! JSONSerialization.jsonObject(with: data)
+        self.hashDict = jsonResult as? [String:String] ?? [:]
+        
     }
     
     public func decrypt(shaHash: String) -> String? {
